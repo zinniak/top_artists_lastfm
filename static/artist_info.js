82417377170868c9a6dbc10932a870f08e5bd885
Zinnia Khan <zinniak@umich.edu>
2019-04-16 23:12:34 -0400

    javascript

--- static/artist_info.js
+++ static/artist_info.js
@@ -1,10 +1,4 @@
 
-function getBio(artist){
-  var current_artist = document.querySelector('.bio');
-  current_artist.innerHTML = artist;
-  console.log(current_artist)
-}
-
 function getPic(artist){
   var current_artist_pic = document.querySelector('.currentpic');
   current_artist_pic.src = artist;
